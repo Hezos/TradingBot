c6df68e90ac2470dc9c78245dee9117c0628e686
Nagy Bence <nbence0620@gmail.com>
2025-08-04 18:36:58 +0200

    Added Repeating DCA calculator to the project.

--- DollarCostAveraging.py
+++ DollarCostAveraging.py
@@ -4,4 +4,16 @@ def DollarCostAveraging(low, high, buy1, buy2):
     X = low * high * ( buy2 + buy1 ) / ( high * buy2 + buy1 * low )
     return X
 
+#This function expects a very long price fall
+def DollarCostAveragingRepeat(low:[], high, buy1, buy2):
+    X = 0
+    High = high
+    for i in range(0, len(low) - 1):
+        High = DollarCostAveraging(low[i], High,buy1, buy2)
+        X = DollarCostAveraging(low[i+1], High,buy1, buy2)
+    return X 
+
 print(DollarCostAveraging(1.1, DollarCostAveraging(1.09, 1.36, 50000, 100000),50000,100000) )
+print(DollarCostAveragingRepeat([0.7,0.6,0.5,0.4,0.3],1.36, 50000, 100000))
+
+
